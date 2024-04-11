7c156dc63b94c85b535b117b88b70985b66eea73
JJB237 <123224197+JJB237@users.noreply.github.com>
2024-04-11 16:53:49 -0400

    Comments to py version

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,10 +1,10 @@
-a, b = 0, 1
+a, b = 0, 1  # Define starting values
 sum_even = 0
-while b < 4000000:
-    if b % 2 == 0:
-        sum_even += b
-    a, b = b, a+b
+while b < 4000000: # Sets upper limit
+    if b % 2 == 0: # Uses the fact that all even numbers are divisible by 2 to check it is even
+        sum_even += b # Adds b to the sum
+    a, b = b, a+b # Updates the values
 
-print(sum_even)
+print(sum_even) # Prints the result
 
 # answer should be 4613732
