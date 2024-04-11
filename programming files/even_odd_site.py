de331459c7a33f041a9ae46ae9aff493854abc7c
Matt-Swaim <112338742+Matt-Swaim@users.noreply.github.com>
2024-04-11 16:46:08 -0400

    Update even_odd_site.py

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -1,13 +1,16 @@
 with open("numbers.html", "w") as f:
+    # creates two tables, one for the even numbers and one for odd numbers
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
     for i in range(1, 50):
+        # even number
         if i % 2 == 0:
             f.write("<tr><td>{}</td><td></td></tr>\n".format(i))
         else:
+            # odd number
             f.write("<tr><td></td><td>{}</td></tr>\n".format(i))
     f.write("</table>\n</body>\n</html>")
-
+# prints out numbers.html after the numbers are added to the file
 with open("numbers.html", "r") as f:
     print(f.read())
     
